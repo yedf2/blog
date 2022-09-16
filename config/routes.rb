e767411390ec8bf02da0ea449db6eb5ffd536a3c
yedf2 <120050102@qq.com>
2022-09-16 09:39:04 +0800

    articles ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :articles
   # Defines the root path route ("/")
   root 'welcome#index'
 end
