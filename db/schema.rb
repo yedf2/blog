d19b30c564b5492ebd990ccf4287ea0c6dd17120
yedf2 <120050102@qq.com>
2022-09-16 10:01:27 +0800

    users seems ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_085356) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_16_015713) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
